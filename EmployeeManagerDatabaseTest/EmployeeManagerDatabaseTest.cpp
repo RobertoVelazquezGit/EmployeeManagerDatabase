95cbb70096edaf8f7bf628f6a2bc1dc170a39405
RobertoVelazquezGit <vmroberto@hotmail.es>
2026-02-01 12:46:29 +0100

    Commentes explaining the solution structure

--- EmployeeManagerDatabaseTest/EmployeeManagerDatabaseTest.cpp
+++ EmployeeManagerDatabaseTest/EmployeeManagerDatabaseTest.cpp
@@ -158,6 +158,9 @@ TEST(TestEmployeeManager, TestGetSalaryInRange)
     }
 }
 
+// This is a console c++ project with gtest and gmock configured
+// Nugget packages used: googletestmock.v.141  
+// EmployeeManagerDatabaseLibrary (custom static library project containing EmployeeManager, Employee, IDatabaseConnection)
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
